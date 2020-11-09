45843bdd29f4fece38cae015781c6647471c96c0
caiovini980 <caiovini.aa@gmail.com>
2020-11-09 16:12:47 -0300

    MouseFollow and PlayerController scripts

---
+++ Table Soccer 3D/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    private float offset = -0.8f;
+    private Vector3 tempPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current mouse position
+        tempPosition = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)
+            );
+
+        //move the object to the mouse position
+        transform.position = new Vector3(tempPosition.x, tempPosition.y, offset);
+    }
+}
---
+++ Table Soccer 3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject mousePointA; //the green ball
+    public GameObject mousePointB; //the orange ball
+    public GameObject arrow;
+    public GameObject circle;
+
+    //Calculate distance
+    private float currentDistance;
+    private float safeSpace;
+    private float shotPower;
+
+    public float maxDistance = 2f;
+
+    private Vector3 shootDirection;
+
+    private void OnMouseDrag()
+    {
+        currentDistance = Vector3.Distance(mousePointA.transform.position, transform.position);
+
+        if (currentDistance <= maxDistance)
+        {
+            safeSpace = currentDistance;
+        }
+        else
+        {
+            currentDistance = maxDistance;
+        }
+
+        //calculate power and direction
+        shotPower = Mathf.Abs(safeSpace) * 10;
+
+        Vector3 dimensionXY = mousePointA.transform.position - transform.position;
+        float difference = dimensionXY.magnitude;
+        mousePointB.transform.position = transform.position + ((dimensionXY / difference) * currentDistance * - 1);
+        mousePointB.transform.position = new Vector3(
+            mousePointB.transform.position.x, 
+            mousePointB.transform.position.y, 
+            -0.8f
+            );
+    }
+
+}
