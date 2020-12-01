394c92907ad78f6920f36c09f0fa6e0464632402
caiovini980 <caiovini.aa@gmail.com>
2020-12-01 11:41:55 -0300

    Add the function to add and show score on the game

--- Table Soccer 3D/Assets/Scripts/BallTrigger.cs
+++ Table Soccer 3D/Assets/Scripts/BallTrigger.cs
@@ -7,10 +7,13 @@ public class BallTrigger : MonoBehaviour
     private string Goal2Tag = "Goal2";
     private string Goal1Tag = "Goal1";
 
+    public UIManager _uiManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 7, 0);        
+        float randomY = Random.Range(-7f, 7f);
+        transform.position = new Vector3(0, randomY, 0);        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,18 +21,21 @@ public class BallTrigger : MonoBehaviour
         if (other.CompareTag(Goal2Tag))
         {
             //Add point to player 1 
+            _uiManager.UpdateScore1();
             Debug.Log("Player 1 scored");
-            transform.position = new Vector3(0, 7, 0);
+            float randomY = Random.Range(-7f, 7f);
+            transform.position = new Vector3(0, randomY, 0);
             //return players for the original position
         }
 
         if (other.CompareTag(Goal1Tag))
         {
             //Add point to player 2
+            _uiManager.UpdateScore2();
             Debug.Log("Player 2 scored");
-            transform.position = new Vector3(0, 7, 0);
+            float randomY = Random.Range(-7f, 7f);
+            transform.position = new Vector3(0, randomY, 0);
             //return players for the original position
         }
     }
-
 }
---
+++ Table Soccer 3D/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text score1, score2;
+    private int points1, points2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points1 = 0;
+        points2 = 0;
+
+        score1.text = "" + 0;
+        score2.text = "" + 0;
+    }
+
+    public void UpdateScore1()
+    {
+        points1++;
+        score1.text = "" + points1;
+    }
+
+    public void UpdateScore2()
+    {
+        points2 ++;
+        score2.text = "" + points2;
+    }
+}
